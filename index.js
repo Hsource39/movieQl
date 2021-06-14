a7b3f358e9df6fa8635dc978b88a8e14cb72fac5
Hsource39 <source390@gmail.com>
2021-06-14 21:05:31 +0900

    Graphql 0.8

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers.js"
 
 const server = new GraphQLServer({
-
+    typeDefs: "./graphql/schema.graphql",
+    resolvers
 });
 
 server.start(() => console.log("Graghql Server Running"))
